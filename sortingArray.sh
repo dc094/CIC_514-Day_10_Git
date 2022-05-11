3f2f8ad5d25fd1b9e22088f8287bdc70e155a785
Durgesh094 <durgeshchavan1994@gmail.com>
2022-05-11 09:01:26 +0530

    a+b*c computation

--- sortingArray.sh
+++ sortingArray.sh
@@ -1,11 +1,13 @@
 #!/bin/bash
 
-echo " Welcome to Shell Scripting Program: Sorting an Array";
+echo " Welcome to Shell Scripting Program: Computation Program";
 
 # Take three user input
-read -p "Enter the value of a: "a;
-read -p "Enter the value of b: "b;
-read -p "Enter the value of c: "c;
+read -p "Enter the value of a:" a;
+read -p "Enter the value of b:" b;
+read -p "Enter the value of c:" c;
 
+expr[0]=$(($a+$b*$c));
 
+echo ${expr[0]};
 
